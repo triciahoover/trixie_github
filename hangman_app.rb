92583fe0eee013f38a0a4ace5af0f8062abc7ebd
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-08-03 09:13:53 -0400

    Marvin fixed everything

--- hangman_app.rb
+++ hangman_app.rb
@@ -1,46 +1,51 @@
 require 'sinatra'
 require_relative 'hmlogic.rb'
-
-
+# i moved wrong guess outside of the function because it was being reset every time the run program was called.!!!!
+$wrong_guesses_count = 0
+$all_guesses = []
 get '/'  do
-	redirect '/splash'
+    redirect '/splash'
 end
-
 get '/splash' do
-	erb :splashscreen
+    erb :splashscreen
 end
-
 get '/gameplay' do
-	erb :gameplay
+    erb :gameplay
 end
-
 post '/game' do
-	$secret_word = params[:secret_word] #secret word comes from gameplay.erb
-	# puts "secret_word is #{$secret_word}"
-	$display_word = set_display_word($secret_word) #calls function from hmlogic.rb
-	# puts "display_word is #{display_word}"
-	redirect '/choices'
+    $secret_word = params[:secret_word] #secret word comes from gameplay.erb
+    # puts "secret_word is #{$secret_word}"
+    $display_word = set_display_word($secret_word) #calls function from hmlogic.rb
+    # puts "display_word is #{display_word}"
+    redirect '/choices'
 end
-
 get '/choices' do
-	message = params[:message] || "" #double goal post is OR operator, uses logic from hmlogic.rb to determine message
-	# $secret_word = params[:secret_word]
-	# display_word = set_display_word($secret_word).to_s
-	erb :game, locals: {display_word: $display_word, message: message, wrong_guesses_count: $wrong_guesses_count}
+    message = params[:message] || "" #double goal post is OR operator, uses logic from hmlogic.rb to determine message
+    # display_word is a var set to the return of function set_display_word and arg is $secret_word / no string needed because we are not passing through redirect.....
+    #!!!!!!!!!!!!!!!!!!!
+    #$display_word is already set in post game above. leaving here resets value every time u are in get choices.
+    # $display_word = set_display_word($secret_word)
+    #!!!!!!!!!!!!!!!!!!!!
+    #notice how i pass in already guessed to show letters already picked- makes for better user experience
+    erb :game, locals: {display_word: $display_word, message: message, wrong_guesses_count: $wrong_guesses_count, already_guessed: $all_guesses }
 end
-
 post '/choices' do
-	choice = params[:choices]
-	run_program(choice)
-	# run_program()=params[:run_program()]
-	redirect '/choices'
+    choice = params[:choices]
+    run_program(choice)
+    
+    redirect '/choices'
 end
-
-
+# i added winner and loser route here
+get'/winner' do
+    "you are a winner"
+end
+get'/loser' do 
+    "your are a loser"
+end
+####### end winner and loser routes
 post '/gameover' do
-	erb :gameover
+    erb :gameover
 end
-
 get '/endscreen' do
-	erb :gameover
+    erb :gameover
 end
--- hmlogic.rb
+++ hmlogic.rb
@@ -15,18 +15,18 @@ end
 #         false
 #     end
 # end
-def push_correct_guess_into(letter) #this function pushes a correct guess, letter, into an array
-    correct_letter = [] 
-    correct_letter.push letter
-    # puts "#{correct_letter} is correct_letter"
-    correct_letter #this calls the array so it is the last step in the function
-end
-def push_incorrect_guess_into(letter) #see above
-    wrong_letter = []
-    wrong_letter.push letter
-    # puts "#{wrong_letter} is wrong_letter" 
-    wrong_letter #this calls the array so it is the last step in the function
-end
+# def push_correct_guess_into(letter) #this function pushes a correct guess, letter, into an array
+#     correct_letter = [] 
+#     correct_letter.push letter
+#     # puts "#{correct_letter} is correct_letter"
+#     correct_letter #this calls the array so it is the last step in the function
+# end
+# def push_incorrect_guess_into(letter) #see above
+#     wrong_letter = []
+#     wrong_letter.push letter
+#     # puts "#{wrong_letter} is wrong_letter" 
+#     wrong_letter #this calls the array so it is the last step in the function
+# end
 def set_display_word(secret_word) #this function displays the secret word with blanks 
     $display_word = [] #sets the variable equal to an empty array
     get_secret_word_length($secret_word).times do #run this function as many times as there are letters in my secret word 
@@ -42,7 +42,8 @@ end
 def update_display_word(secret_word, letter)
     p "secret word is #{secret_word} and letter is #{letter}"
     # display_word = set_display_word(secret_word) #display word is blanks
-    $secret_word_array = $secret_word.split("") #this breaks the secret word into an array of individual letters 
+    $secret_word_array = secret_word.split("") #this breaks the secret word into an array of individual letters 
+    p "secret array is #{$secret_word_array}"
     counter = 0
     found = false
     $secret_word_array.each do |index|#we used the .each so that each letter in the secret word gets compared to the letter (index) ;the input within the bars is another variable which allows us to pass the letters into the do loop. technically index is the input for each letter
@@ -50,6 +51,7 @@ def update_display_word(secret_word, letter)
         $display_word[counter] = letter
         found = true
         end 
+        p"new display is #{$display_word}"
     counter = counter + 1
     end
     if found == false
@@ -62,40 +64,42 @@ end
 def check_winner(display_word, secret_word_array)
     if $display_word == $secret_word_array
         $solved = true
+        redirect'/winner'
         # p "Winner, Winner, Chicken Dinner!"
     end
     if $wrong_guesses_count >= 10 
+        redirect'/loser'
         # puts "Sorry, you lose!"
     end
 end
 def you_lost()
     "Uh Oh!"
 end
-def one_player()
-    word_bank = ["puppy", "chicken", "amazon", "baseball", "yeti", "wombat", "racecar", "apple"]
-     $secret_word  = word_bank.shuffle.first
-    end
-def two_player()
-    # p "Enter secret word."
-    $secret_word = gets.chomp #$ creates a global variable. Will need to be added in everywhere the variable is used. Similiar to sessions when not using Sinatra- allows you to pass data from point to point. Does not rely on cookies like sessions does.
-end
-def input_secret_word()
-    # puts "Would you like to play one player or two? Please type one or two."
-    $answer = gets.chomp
-    if $answer == "two"
-        two_player()
-    else
-        one_player()
-    end
-end
-def input_letter()
-    # p "Input letter"
-    $letter = gets.chomp
-    if $all_guesses.include?($letter)
-        # puts "You have already guessed #{$letter} :("
-    else $all_guesses << $letter
-    end
-end
+# def one_player()
+#     word_bank = ["puppy", "chicken", "amazon", "baseball", "yeti", "wombat", "racecar", "apple"]
+#      $secret_word  = word_bank.shuffle.first
+#     end
+# def two_player()
+#     # p "Enter secret word."
+#     $secret_word = gets.chomp #$ creates a global variable. Will need to be added in everywhere the variable is used. Similiar to sessions when not using Sinatra- allows you to pass data from point to point. Does not rely on cookies like sessions does.
+# end
+# def input_secret_word()
+#     # puts "Would you like to play one player or two? Please type one or two."
+#     $answer = gets.chomp
+#     if $answer == "two"
+#         two_player()
+#     else
+#         one_player()
+#     end
+# end
+# def input_letter()
+#     # p "Input letter"
+#     $letter = gets.chomp
+#     if $all_guesses.include?($letter)
+#         # puts "You have already guessed #{$letter} :("
+#     else $all_guesses << $letter
+#     end
+# end
 def play_hangman(secret_word, letter) #bringing all the functions from above into this line. Going to have a do loop and a counter. Function in progress.
     if 
         check_letter?(secret_word, letter) == true
@@ -105,19 +109,30 @@ def play_hangman(secret_word, letter) #bringing all the functions from above int
     end
 end
 def run_program(letter) #caling functions in order
-	p "the letter is #{letter}"
+    p "the letter is #{letter}"
     # p "Player 2: Choose a letter."
     $solved = false
-    $wrong_guesses_count = 0
-    # $all_guesses = []
+    
+    #i am moving wrong_guesses_count so that it doesnt keep resetting to 0
+    # $wrong_guesses_count = 0
+    
+     #all guessses was being reset every pass to empty array
+     # $all_guesses = []
+    
     # #below... as long as "solved" isn't true AND wrong guesses is less than 10, update display and check for winner loops.
-    while $solved == false && $wrong_guesses_count <10 #while solved is false
-        update_display_word($secret_word, letter)
+    # while $solved == false && $wrong_guesses_count <10 #while solved is false
+        if $all_guesses.include?(letter)
+           redirect'/choices?message=already picked... pick again'
+        else
+            $all_guesses << letter 
+            update_display_word($secret_word, letter)
+            check_winner($display_word, $secret_word_array)
+        end
         # check_winner($display_word, $secret_word_array)
      #     if $all_guesses.include?(letter) != true
-        	# redirect'/choices?message=already picked... pick again'
+            # redirect'/choices?message=already picked... pick again'
      #    # puts "You have already guessed #{$letter} :("
-    	# else $all_guesses << letter
-    	end
+        # else $all_guesses << letter
+        # end
     # end
 end
